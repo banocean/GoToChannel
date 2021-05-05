2672d250eed8ce4d49946c55f352088ab1ad1aba
oceaann <kontakt.ocaen@gmail.com>
2021-05-05 12:16:07 +0200

    (fix): Run code only in live chat iframe/window

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
-const script = document.createElement('script');
-script.src = chrome.runtime.getURL('modules/runMain.js');
-document.head.appendChild(script);
+if(window.location.pathname.includes("live_chat")) {
+    const script = document.createElement('script');
+    script.src = chrome.runtime.getURL('modules/runMain.js');
+    document.head.appendChild(script);
+} else {
+    // work in progress
+}
